7234e38992cf5df78b9effdecea69de1fa3c5f27
sadmiraga <sadmirvela@gmail.com>
2020-08-31 04:55:43 +0200

    website live, fixed links

---
+++ LIVE ATEC/js/navbar.sticky.js
@@ -0,0 +1,16 @@
+let navigation = document.getElementById("header");
+let logo = document.getElementById("logo");
+
+document.body.onscroll = function () {
+  console.log(document.documentElement.scrollTop);
+  if (
+    document.body.scrollTop > 130 ||
+    document.documentElement.scrollTop > 130
+  ) {
+    navigation.style.height = "70px";
+    logo.style.fontSize = "2rem";
+  } else {
+    navigation.style.height = "130px";
+    logo.style.fontSize = "3rem";
+  }
+};
